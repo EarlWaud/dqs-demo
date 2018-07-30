5229c6fc39339395a3491e00958e7a9f30a566e5
ewaud <earl_waud@intuit.com>
2018-07-30 16:42:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('demo-app');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+// combined, tiny
+app.use(morgan('tiny'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+app.listen(3000, function(){
+    debug(`listening on port ${chalk.green(3000)}`);
+});
